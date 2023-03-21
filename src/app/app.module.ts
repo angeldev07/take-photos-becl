7045deff8946584ad985e81c025e13185623866b
angedev07 <angelgaran20@gmail.com>
2023-03-21 17:59:07 -0500

    finalizado estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { WebcamModule } from 'ngx-webcam'
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { WebcamModule } from 'ngx-webcam'
   ],
   imports: [
     BrowserModule,
-    WebcamModule
+    WebcamModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/take-photo.service.ts
+++ src/app/services/take-photo.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class TakePhotoService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+  enviarImg(){
+    this.http.post('​http://localhost:5000/api/imageSave', {
+      img: '',
+      codigo: '1152069'
+     })
+  }
 }
