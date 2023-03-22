eac81b056bdef05ef546a0e8f8148367c23d6240
angeldev07 <angelgaran20@gmail.com>
2023-03-21 22:35:28 -0500

    Creado un observable para indicar cuando disparar el evento para tomar foto

--- src/app/services/take-photo.service.ts
+++ src/app/services/take-photo.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
+import { BehaviorSubject } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class TakePhotoService {
 
+  baseUrl = 'http://localhost:5000/api/imageSave' 
+  trigger = new BehaviorSubject<boolean>(false);
   constructor(
     private http: HttpClient
   ) { }
 
+  
+  takeSnapshot(){
+    this.trigger.next(true)
+    return this.trigger.asObservable();
+  }
 
-  enviarImg(){
-    this.http.post('​http://localhost:5000/api/imageSave', {
-      img: '',
-      codigo: '1152069'
-     })
+  enviarImg(imgBase64: string, studentCode: string){
+    const body = {
+      imgBase64,
+      studentCode
+    }
+    return this.http.post(this.baseUrl, body)
   }
 }
